70cbfd138e169e1d4af70b9248f0027632cbd481
Katherine-Be <katherine.bensley.93@gmail.com>
2024-07-07 12:29:07 -0400

    about me formatted

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,16 +2,17 @@ import React from 'react';
 import avatar from '../assets/portraitalt1.png';
 function About() {
     return (
-        <section >
+        <section>
+            <div>
             <h2 class="ml-5 lg:ml-10 xl:ml-20 2xl:ml-96 text-4xl md:text-6xl" >About Me</h2>
-            <div class="absolute -mt-48 sm:-mt-20 lg:-mt-0 w-full flex justify-end sm:justify-center">
+            <div class="-mt-48 sm:-mt-20 lg:-mt-0 w-full flex justify-end sm:justify-center">
                 <figure class="h-48 w-48 sm:h-56 sm:w-56 lg:h-80 lg:w-80"> {/*need to push the figure up and to the right on small screens!!!!!*/}
                     <img src={avatar} class="rounded-full object-cover h-full w-full shadow-md" />
                 </figure>
             </div>
-
-            <div  class="flex-wrap lg:flex-row">  
-                <div class="basis-full lg:basis-1/3 group w-64 h-96 bg-white dark:bg-shadesOfBlue shadow-md relative rounded-lg m-4 hover:ring hover:ring-shadesOfBlue dark:hover:ring-white transform duration-500 hover:scale-105 hover:shadow-xl">
+</div>
+            <div  class="xs:flex-wrap lg:flex  lg:justify-around">  
+                <div class="lg:basis-1/3 group h-100 bg-white dark:bg-shadesOfBlue shadow-md relative rounded-lg m-16 hover:ring hover:ring-shadesOfBlue dark:hover:ring-white transform duration-500 hover:scale-105 hover:shadow-xl">
                     <div class="bg-shadesOfBlue flex justify-center items-center w-28 h-28 rounded-full absolute mx-auto right-0 left-0 -inset-y-14 border-4 border-slate-200 dark:border-slate-800 group-hover:bg-white dark:group-hover:bg-shadesOfBlue group-hover:shadow-md transform duration-300">
                         <span class="text-3xl md:text-5xl text-white dark:text-slate-800 group-hover:text-shadesOfBlue dark:group-hover:text-white transform duration-300">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-10">
@@ -19,17 +20,16 @@ function About() {
                             </svg>
                         </span>
                     </div>
-                    <div class="flex flex-col items-center justify-center absolute mx-auto inset-0 p-4">
+                    <div class="flex flex-col items-center justify-center mx-auto inset-0 p-4">
                         <div>
                             <h2 class="text-xl capitalize font-bold text-shadesOfBlue dark:text-slate-800 my-4 my-8">As A Team Leader
                             </h2>
                         </div>
                         <div>
-                            <p class="text-center text-base dark:text-white">I have 11 years of experience leading teams in multiple industries. In my my early years, I built teams and operations
-                                for several new local businesses which grew to succeed and break even within their first year. Most recently, I 
+                            <p class="text-center text-base">11 years ago I started building teams and operations
+                                for several local businesses. Most recently, I 
                                 had the opportunity to expand an existing team, hiring on 18 new members, and can tout a 100% retention rate of team members and reaching 150% of 
-                                the financial goal for that season. I am keen on motivation analysis and empathetic communication. My teams know that
-                                I am there to be a resource with their best interests at heart. My aim is to create a habit of success ensured by skill
+                                the financial goal for that season. I am keen on motivation analysis and empathetic communication. My aim is to create a habit of success ensured by skill
                                 development and thorough communication.
                             </p>
                         </div>
@@ -39,7 +39,7 @@ function About() {
                     </div>
                 </div>
 
-                <div class="basis-full lg:basis-1/3 group w-64 h-96 bg-white dark:bg-shadesOfBlue shadow-md relative rounded-lg m-4 hover:ring hover:ring-shadesOfBlue dark:hover:ring-white transform duration-500 hover:scale-105 hover:shadow-xl">
+                <div class="lg:basis-1/3 group h-100 bg-white dark:bg-shadesOfBlue shadow-md relative rounded-lg m-16 hover:ring hover:ring-shadesOfBlue dark:hover:ring-white transform duration-500 hover:scale-105 hover:shadow-xl">
                     <div class="bg-shadesOfBlue flex justify-center items-center w-28 h-28 rounded-full absolute mx-auto right-0 left-0 -inset-y-14 border-4 border-slate-200 dark:border-slate-800 group-hover:bg-white dark:group-hover:bg-shadesOfBlue group-hover:shadow-md transform duration-300">
                         <span class="text-3xl md:text-5xl text-white dark:text-slate-800 group-hover:text-shadesOfBlue dark:group-hover:text-white transform duration-300">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-10">
@@ -47,13 +47,13 @@ function About() {
                             </svg>
                         </span>
                     </div>
-                    <div class="flex flex-col items-center justify-center absolute mx-auto inset-0 p-4">
+                    <div class="flex flex-col items-center justify-center mx-auto inset-0 p-4">
                         <div>
                             <h2 class="text-xl capitalize font-bold text-shadesOfBlue dark:text-slate-800 my-4 my-8">As A Creator
                                 </h2>
                         </div>
                         <div>
-                            <p class="text-center text-base dark:text-white"><em>Success is a science; if you have the conditions, you get the result.</em><br />
+                            <p class="text-center text-base"><em>Success is a science; if you have the conditions, you get the result.</em><br />
                                 - Letters of Oscar Wilde
                             </p>
                             <p>Mathematically this is known as "likelihood" - it's a fun concept to mull over. 
@@ -68,7 +68,7 @@ function About() {
                     </div>
                 </div>
 
-                <div class="basis-full lg:basis-1/3 group w-64 h-96 bg-white dark:bg-shadesOfBlue shadow-md relative rounded-lg m-4 hover:ring hover:ring-shadesOfBlue dark:hover:ring-white transform duration-500 hover:scale-105 hover:shadow-xl">
+                <div class="lg:basis-1/3 group w-64 h-100 bg-white dark:bg-shadesOfBlue shadow-md relative rounded-lg m-16 hover:ring hover:ring-shadesOfBlue dark:hover:ring-white transform duration-500 hover:scale-105 hover:shadow-xl">
                     <div class="bg-shadesOfBlue flex justify-center items-center w-28 h-28 rounded-full absolute mx-auto right-0 left-0 -inset-y-14 border-4 border-slate-200 dark:border-slate-800 group-hover:bg-white dark:group-hover:bg-shadesOfBlue group-hover:shadow-md transform duration-300">
                         <span class="text-3xl md:text-5xl text-white dark:text-slate-800 group-hover:text-shadesOfBlue dark:group-hover:text-white transform duration-300">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-10">
@@ -76,13 +76,13 @@ function About() {
                             </svg>
                         </span>
                     </div>
-                    <div class="flex flex-col items-center justify-center absolute mx-auto inset-0 p-4">
+                    <div class="flex flex-col items-center justify-center mx-auto inset-0 p-4">
                         <div>
                             <h2 class="text-xl capitalize font-bold text-shadesOfBlue dark:text-slate-800 my-4 my-8">As A Developer
                             </h2>
                         </div>
                         <div>
-                            <p class="text-center text-base dark:text-white">As an interactive developer, my primary aim is to create an experience and 
+                            <p class="text-center text-base">As an interactive developer, my primary aim is to create an experience and 
                                 communicate an understanding of the user while effectively representing a 
                                 brand's voice in the applications I build. I am versed in numerous web development technologies including, but not limited to - 
                                 HTML, CSS, JavaScript, Bootstrap, Node, Express, and MySQL. My current studies include 
