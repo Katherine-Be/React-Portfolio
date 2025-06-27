e052faa9ca89f175b161ccc48590b49606e25248
Katherine-Be <katherine.bensley.93@gmail.com>
2025-06-27 15:18:39 -0400

    getting rid of contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -71,8 +71,8 @@ function Contact() {
                 <span class="text-sm">ka.alex.bensley@gmail.com</span>
             </a>
 
-        </div>
-        <form class="md:col-span-8 p-10">
+      </div>
+{/*          <form class="md:col-span-8 p-10">
             <div class="flex flex-wrap -mx-3 mb-6">
                 <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
                     <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
@@ -141,8 +141,9 @@ function Contact() {
             </div>
 
         </form>
-
+ */}
     </div>
+   
 </div>
 
 
